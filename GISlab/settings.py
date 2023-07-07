fbe17cebfb5cf3792cb406038bd071f922ca24fb
maso0310 <balimon@hotmail.com>
2023-07-07 13:17:42 +0800

    更新build.sh

--- GISlab/settings.py
+++ GISlab/settings.py
@@ -80,12 +80,12 @@ WSGI_APPLICATION = 'GISlab.wsgi.application'
 # https://docs.djangoproject.com/en/3.0/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        # Feel free to alter this value to suit your needs.
-        default='postgresql://postgres:postgres@localhost:5432/mysite',
-        conn_max_age=600
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
